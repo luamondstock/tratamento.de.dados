63fcd2d17d3d9f52870ab5e9a35deed5a3f7e8ca
luamondstock <134516705+luamondstock@users.noreply.github.com>
2024-09-25 09:22:39 -0300

    Add files via upload

--- graficos/dadosGerais.js
+++ graficos/dadosGerais.js
@@ -1,1 +1,10 @@
 const url = 'https://raw.githubusercontent.com/luamondstock/API2024/refs/heads/main/entrevista.json'
+
+async function vizualizarInformacoes(){
+    const res = await fetch(url)
+    const dados = await res.json()
+
+    console.log(dados.total_entrevistados)
+}
+    
+vizualizarInformacoes()
