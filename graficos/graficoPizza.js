18dd870d08313572514542717f0634cc65489d54
luamondstock <134516705+luamondstock@users.noreply.github.com>
2024-10-03 12:01:00 -0300

    Add files via upload

--- graficos/graficoPizza.js
+++ graficos/graficoPizza.js
@@ -6,11 +6,24 @@ async function criarGraficoPizza(){
     const dados = await res.json()
     const livros = Object.keys(dados)
     const valores = Object.values(dados)
-                       
-    for(let i = 0; i <= dados.length; i++){
-        console.log(Object)
-        
+
+    const data = [ 
+      {
+        values: valores,
+        labels: livros,
+        type: 'pie', 
+        textinfo: 'label + percent', 
+      }
+    ]
+    const layout = {
+        height: 400, 
+        width: 600, 
+        plot_bgcolor: pegarCSS('--cor02'),
+        paper_bgcolor: pegar('--cor03')
     }
+    const livrosTitulo = document.createElement("h3")
+    livrosTitulo.classList.add("caixa-grafico__titulo")
+    livrosTitulo.innerHTML = ' Os <span>livros</span> mais vendidos da editora seguinte em 2023'
 }
 
 criarGraficoPizza()
